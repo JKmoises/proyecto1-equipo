f6a1352539bf6c3278a04aeb20ffc074e6dc8a1a
Walter Nikolas <sanchez.walter@gmail.com>
2024-01-20 00:17:11 -0300

    Corregir fadeOut

--- js/app.js
+++ js/app.js
@@ -7,13 +7,10 @@ window.onload = function () {
   openNav.addEventListener("click", function () {
     headerNav.classList.remove("fadeOut")
     headerNav.classList.add("fadeIn")
-    headerNav.style.visibility = "visible"
   })
 
   closeNav.addEventListener("click", function () {
-    /** NO FUNCIONA el fadeOut; visibility = hidden se ejecuta inmediatamente */
     headerNav.classList.remove("fadeIn")
     headerNav.classList.add("fadeOut")
-    headerNav.style.visibility = "hidden"
   })
 }
