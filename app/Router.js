505607a758d2ec89fea33b2d013f7847e9e43a24
Justin Huertas <justin.hv08@gmail.com>
2024-01-20 01:12:13 -0500

    Terminar pagina Tienda SPA

--- app/Router.js
+++ app/Router.js
@@ -1,23 +1,34 @@
-import { ajax } from "./helpers/ajaxHelper.js";
-import { Store } from "./pages/Store.js";
+import { TitlePage } from './components/TitlePage.js';
+import { ajax } from './helpers/ajaxHelper.js';
+import { Store } from './pages/Store.js';
 
 export async function Router() {
-  const { hash } = location;
+	const { hash } = location;
 
-  const $main = document.querySelector("#main");
+	const $main = document.querySelector('#main');
+	$main.classList.remove('main');
 
-  if (!hash || hash === "#/") {
-    console.log("Inicio");
-  }else if (hash === "#/tienda") {
-    console.log("Tienda");
+	if (!hash || hash === '#/') {
+		console.log('Inicio');
+	} else if (hash === '#/tienda') {
+		$main.classList.add('main');
 
-  }else if (hash === "#/sobre-mi") {
-    console.log("Sobre mi");
-  }else if (hash === "#/contacto") {
-    console.log("Contacto");
-  }else if (hash === "#/detalle-producto") {
-    console.log("Detalle Producto");
-  } else {
-    console.log("Error 404");
-  }
+		$main.append(TitlePage('/ elige tus galletas'));
+
+		//Obtener Datos del JSON
+		await ajax({
+			url: 'http://localhost:3000/products',
+			cbSuccess: (products) => {
+				$main.append(Store(products));
+			},
+		});
+	} else if (hash === '#/sobre-mi') {
+		console.log('Sobre mi');
+	} else if (hash === '#/contacto') {
+		console.log('Contacto');
+	} else if (hash === '#/detalle-producto') {
+		console.log('Detalle Producto');
+	} else {
+		console.log('Error 404');
+	}
 }
