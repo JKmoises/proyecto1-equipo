22d1b29ebc57f5a9494c3557fe993b42161fe54a
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-01-25 13:22:52 -0300

    codigo javascript SPA de inicio seccion 'compra-envio-envoltura'

---
+++ app/components/StartOtherServices.js
@@ -0,0 +1,45 @@
+export const SoldSubmitGift = () => {
+let $SoldSubmitGift = document.createElement("section")
+  $SoldSubmitGift.classList.add("shopping-section")
+  $SoldSubmitGift.alt = "Services"
+
+  $SoldSubmitGift.innerHTML = /*html*/
+  `
+  <article class="shopping-section__item flex-col-center">
+    <img src="./assets/icono-compras.svg" alt="icono-compras" class="shopping-section__icon">
+    <h3 class="shopping-section__title text-uppercase">
+      comprar y recoger
+    </h3>
+    <p class="shopping-section__description">
+      Llévatelas recién hechas
+    </p>
+  </article>
+  
+  <article class="shopping-section__item flex-col-center">
+    <img src="./assets/icono-envio.svg" alt="icono-envio" class="shopping-section__icon">
+    <h3 class="shopping-section__title text-uppercase">
+      envio gratis
+    </h3>
+    <p class="shopping-section__description">
+      Entrega a domicilio
+    </p>
+  </article>
+  
+  <article class="shopping-section__item flex-col-center">
+    <img src="./assets/icono-envolturas.svg" alt="icono-envolturas" class="shopping-section__icon">
+    <h3 class="shopping-section__title text-uppercase">
+      envolturas para regalo
+    </h3>
+    <p class="shopping-section__description">
+      Envía una dulce sorpresa
+    </p>
+  </article>
+  `
+  
+return $SoldSubmitGift
+
+}
+
+
+
+
